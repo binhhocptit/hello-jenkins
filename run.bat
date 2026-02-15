ffcbcf73decb01868f253316860c6c1bd8c6a53f
binhhocptit <BinhNT.B21CN019@stu.ptit.edu.vn>
2026-02-15 12:54:24 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,19 @@
 @echo off
 echo ===== RUN APP =====
 
+cd /d C:\ProgramData\Jenkins\.jenkins\workspace\demo-github
+
+REM đường dẫn pm2 thật
+set PM2=C:\Users\Admin\AppData\Roaming\npm\pm2.cmd
+
 echo Stop old app...
-pm2 delete jenkins-app >nul 2>&1
+%PM2% delete jenkins-app >nul 2>&1
 
 echo Start new app...
-pm2 start app.js --name jenkins-app
+%PM2% start app.js --name jenkins-app
 
-pm2 save
+echo Save process list...
+%PM2% save
 
-echo App deployed!
+echo ===== DEPLOY SUCCESS =====
 exit /b 0
