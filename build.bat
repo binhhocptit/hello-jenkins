f9542184b7d4e93458588e27ea8901ff2d61f247
binhhocptit <BinhNT.B21CN019@stu.ptit.edu.vn>
2026-02-15 10:26:52 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 @echo off
-echo ===== BUILD LAN 2 HEHE FROM JENKINS =====
-echo %date% %time%
+echo Running tests...
+
+dir abcxyznotexist >nul 2>nul
+if %errorlevel% neq 0 (
+    echo BUILD FAILED - TEST FAILED
+    exit /b 1
+)
+
+echo BUILD SUCCESS
