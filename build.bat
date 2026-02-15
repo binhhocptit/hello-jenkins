432fb54dae89b45aaa9af564da8164482b42bacd
binhhocptit <BinhNT.B21CN019@stu.ptit.edu.vn>
2026-02-15 12:32:53 +0700

    Improve app management in build.bat

--- build.bat
+++ build.bat
@@ -9,8 +9,13 @@ call npm test
 if %errorlevel% neq 0 exit /b 1
 
 echo Stopping old app...
-pm2 stop app >nul 2>&1
-pm2 delete app >nul 2>&1
+pm2 describe app >nul 2>&1
+if %errorlevel%==0 (
+    pm2 stop app
+    pm2 delete app
+) else (
+    echo No existing app found.
+)
 
 echo Starting app with PM2...
 pm2 start app.js --name app
