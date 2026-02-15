7a6be59e39f9505b13605a772c757b25e845b5e2
binhhocptit <BinhNT.B21CN019@stu.ptit.edu.vn>
2026-02-15 12:57:30 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,13 +4,15 @@ echo ===== RUN APP =====
 cd /d C:\ProgramData\Jenkins\.jenkins\workspace\demo-github
 
 echo Stop old app...
-C:\Users\Admin\AppData\Roaming\npm\pm2.cmd delete jenkins-app >nul 2>&1 || echo No old app
+C:\Users\Admin\AppData\Roaming\npm\pm2.cmd delete jenkins-app >nul 2>&1
 
 echo Start new app...
-C:\Users\Admin\AppData\Roaming\npm\pm2.cmd start app.js --name jenkins-app
+start "" /b C:\Users\Admin\AppData\Roaming\npm\pm2.cmd start app.js --name jenkins-app
+
+timeout /t 2 >nul
 
 echo Save process...
-C:\Users\Admin\AppData\Roaming\npm\pm2.cmd save
+C:\Users\Admin\AppData\Roaming\npm\pm2.cmd save >nul 2>&1
 
 echo ===== DEPLOY SUCCESS =====
 exit /b 0
