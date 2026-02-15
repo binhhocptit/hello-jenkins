b50119eee71f65c33939fb2e76b9c028d4df6554
binhhocptit <BinhNT.B21CN019@stu.ptit.edu.vn>
2026-02-15 10:42:49 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,16 @@
-@echo off
-echo Running tests...
+echo ===== JENKINS PIPELINE START =====
 
-echo ALL TEST PASSED
+echo Installing dependencies...
+call npm install
+
+echo Running tests...
+call npm test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Killing old server...
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :3000') do taskkill /F /PID %%a
+
+echo Starting server...
+start /B npm start
+
+echo ===== DEPLOY DONE =====
